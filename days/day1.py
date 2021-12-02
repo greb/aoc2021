653c83dcb9fed531a2a16908caeaa002ddccc5b6
greb <greb>
2021-12-02 11:59:28 +0100

    Add parsing step to utility script
    * Avoid multiple parsing per solution
    * Looks cleaner (for me)

--- days/day1.py
+++ days/day1.py
@@ -2,16 +2,12 @@ def parse(inp):
     depths = list(map(int, inp.splitlines()))
     return depths
 
-def part1(inp):
-    depths = parse(inp)
+def part1(depths):
     cnt = sum(b > a for (a, b) in zip(depths, depths[1:]))
-
     return cnt
 
 
-def part2(inp):
-    depths = parse(inp)
+def part2(depths):
     cnt = sum(b > a for (a, b) in zip(depths, depths[3:]))
-
     return cnt
 
